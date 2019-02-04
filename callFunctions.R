ca0cc13a6f9734365912198430984bb96e155bdb
Markus Möller <markus.moeller@terrasys.info>
2019-02-04 11:03:14 +0100

    Add files via upload

--- callFunctions.R
+++ callFunctions.R
@@ -11,7 +11,9 @@ IN.DIR <- "_data/"
 #directory containing results
 OUT.DIR <- "_result/"
 #name of reference unit shapefile
-RU.SHP = "RU_fergana"
+MODIS.SHP = "MODIS_fergana"
+#Irrigation mask
+IM.GRD = "IM.sgrd"
 #name of pure sample file
 PS  = "PureSample.csv"
 YEAR = 2015
@@ -20,7 +22,7 @@ print("Import functions")
 #-----------------------------------------------------------------------------------------------------
 source(file.path(W.DIR,FUNC.DIR,"fPackage.R"))
 source(file.path(W.DIR,FUNC.DIR,"fPuSa.R"))
-source(file.path(W.DIR,FUNC.DIR,"fZonaS.R"))
+source(file.path(W.DIR,FUNC.DIR,"fZonaSt.R"))
 source(file.path(W.DIR,FUNC.DIR,"fSample.R"))
 source(file.path(W.DIR,FUNC.DIR,"fPuSaCom.R"))
 source(file.path(W.DIR,FUNC.DIR,"fModisClass.R"))
@@ -32,22 +34,24 @@ print("Carry out functions")
 print("Dissimiliarity test of samples")
 #-------------------------------------------------------------------------------
 ###Function
-d <-       fPuSa(W.DIR,
-           IN.DIR ="_data/",
-           OUT.DIR = "_result/",
-           CLASS.NAME = "CLASS",
-           PS)
+fPuSa(W.DIR=W.DIR,
+      IN.DIR=IN.DIR,
+      OUT.DIR=OUT.DIR,
+      CLASS.NAME = "CLASS",
+      PS=PS)
+
 #-------------------------------------------------------------------------------
 print("Zonal statistics of reference units for MODIS imagery")
 #-----------------------------------------------------------------------------------------------------
 ###Function
-RU.MODIS <- fZonaS(W.DIR,
-                             IN.DIR  = "_data/",
-                             OUT.DIR = "_result/",
-                             RU.SHP = "RU_fergana",
-                             YEAR = YEAR)
+ZS.SHP <- fZonaSt(W.DIR=W.DIR,
+                   IN.DIR=IN.DIR,
+                   OUT.DIR=OUT.DIR,
+                   MODIS.SHP=MODIS.SHP,
+                   IM.GRD,
+                   YEAR = YEAR)
 
-print(RU.MODIS)
+print(ZS.SHP)
 ###Result
 #RU_fergana[YEAR].shp -> shape file with DOY-specific NDVI values 
 #PureSample_NDVI-profiles.pdf -> class-specific NDVI profile plots
@@ -60,12 +64,12 @@ print("Deriving samples for classification applying a comparison of pure sample 
 #ZS.SHP - shape file with DOY-specific NDVI values; result from applying fSample function 
 
 ###Function
-SAMPLE <- fSample(W.DIR,
-                  PS,
-                  IN.DIR = "_data/",
-                  ZS.SHP = paste(RU.SHP,YEAR,sep=""),
-                  RU.SHP = RU.SHP,
-                  OUT.DIR = "_result/",
+SAMPLE <- fSample(W.DIR=W.DIR,
+                  PS=PS,
+                  IN.DIR = IN.DIR,
+                  ZS.SHP = ZS.SHP,
+                  MODIS.SHP = MODIS.SHP,
+                  OUT.DIR = OUT.DIR,
                   Q = 1)
 
 print(SAMPLE$SAMPLE.SHP)
@@ -79,30 +83,16 @@ print(SAMPLE$SAMPLE.AGG)
 #-------------------------------------------------------------------------------
 print("3 | Comparison of Pure Samples and aggregated samples derived with fSample function")
 #-------------------------------------------------------------------------------
-###Additional parameters
-#column name with class names
-CLASS.NAME <- "CLASS"
-#name of pure sample file
-PS1 <- PS
-#name of file containing aggregated sample NDVI profiles
-PS2 <- SAMPLE$SAMPLE.AGG
-#prefix of columns containing DOY-specific NDVI values within PS1
-PS1PF <- "PS"
-#prefix of columns containing DOY-specific NDVI values within PS2
-PS2PF <- "MD"
-#threshold which proportion of class-specific NDVI values should be considered (1=100%)  
-TH <- 0.75
-
 ###Function
-fPuSaCom(W.DIR,
-             IN.DIR,
-             OUT.DIR,
-             CLASS.NAME,
-             PS1,
-             PS2,
-             PS1PF,
-             PS2PF,
-             TH=0.75)
+fPuSaCom(W.DIR=W.DIR,
+         IN.DIR=IN.DIR,
+         OUT.DIR=OUT.DIR,
+         CLASS.NAME="CLASS",
+         PS1=PS,
+         PS2=SAMPLE$SAMPLE.AGG,
+         PS1PF="PS",
+         PS2PF="MD",
+         TH=0.75)
 
 ### C | result
 #PureSample__RU_fergana[YEAR]_SAMPLE-NDVI_agg_Q[Q].pdf - plot of NDVI profiles based on both pure samples and aggregated samples derived with fPuSaDi function
@@ -111,10 +101,10 @@ fPuSaCom(W.DIR,
 print("5 | MODIS classification")
 #-------------------------------------------------------------------------------
 ###Function
-MODIS.CLASS <- fModisClass(W.DIR,
+MODIS.CLASS <- fModisClass(W.DIR=W.DIR,
                            IN.DIR = OUT.DIR,
-                           OUT.DIR,
-                           ZS.SHP = RU.MODIS,
+                           OUT.DIR=OUT.DIR,
+                           ZS.SHP = ZS.SHP,
                            SAMPLE.SHP = SAMPLE$SAMPLE.SHP,
                            PART = 0.75,
                            T.CLASS = "CLASS",
